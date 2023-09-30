1cd449b2e75026e7d12d8812255d79816bf2637c
Luiz F Dias <luizfd@gmail.com>
2023-09-30 18:18:51 -0300

    Header Component update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import './styles.css'
 export default function Header(){
     return(
         <header>
-            <div>
+            <div className='header-content-container'>
                 <h1>Github API</h1>
             </div>
         </header>
