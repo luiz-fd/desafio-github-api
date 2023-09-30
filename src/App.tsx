4338933a7d011a414e12f23e840def87a8dd61e8
Luiz F Dias <luizfd@gmail.com>
2023-09-30 17:01:39 -0300

    Header Component, Home/HomeBody routes, without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import Home from "./routes/home"
+import HomeBody from "./routes/home/homebody"
+
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} >
+            <Route index element={<Navigate to="/home" />} />
+            <Route path="home" element={<HomeBody />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
