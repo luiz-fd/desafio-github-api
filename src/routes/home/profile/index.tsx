3bdeb6eeefa10c21cedd514129e431784afc0471
Luiz F Dias <luizfd@gmail.com>
2023-10-01 12:32:26 -0300

    Profile findByLogin

--- src/routes/home/profile/index.tsx
+++ src/routes/home/profile/index.tsx
@@ -1,18 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "./styles.css";
-import TestImg from "../../../assets/image1.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ProfileDTO } from "../../../models/profile";
+import * as profileService from '../../../services/profile-service'
 
 type FormData = {
   loginName: string;
 };
 
 export default function Profile() {
-  const teste = "oi";
   const [userName, setUserName] = useState(String);
   const [formData, setFormData] = useState<FormData>({
     loginName: "",
   });
+
+  const [profile, setProfile] = useState<ProfileDTO>()
+
+  useEffect(() => {
+    profileService.findByLogin("acenelio")
+    .then(response => {
+      setProfile(response.data);
+      console.log(response.data);
+
+    })
+  },[]);
 
   function handleInputChange(event: any) {
     const value = event.target.value;
@@ -42,23 +53,24 @@ export default function Profile() {
         </div>
       </form>
       <h2>{userName }</h2>
+      {profile && 
       <div className="profile-result-container">
         <div className="profile-result-card">
-          <img src={TestImg} alt="FOTO"></img>
+          <img src={profile.avatar_url} alt="FOTO"></img>
           <div className="profile-info">
             <h4>Informações</h4>
             <input
               type="text"
               placeholder="Perfil: "
               name="Perfil: "
-              value={teste}
+              value={profile.login}
             ></input>
-            <input type="text" placeholder="Seguidores: "></input>
-            <input type="text" placeholder="Localidade: "></input>
-            <input type="text" placeholder="Nome: "></input>
+            <input type="text" placeholder="Seguidores: " value={profile.followers}></input>
+            <input type="text" placeholder="Localidade: " value={profile.location}></input>
+            <input type="text" placeholder="Nome: " value={profile.name}></input>
           </div>
         </div>
-      </div>
+      </div>}
 
       <div className="profile-error-container">
         <h1>Erro ao buscar usuário</h1>
